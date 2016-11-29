d11b5d80d9781ebfcffe121831c5fc156eeec258
NYAN_KAT <thomas.cusson@larez.fr>
2016-11-29 23:08:50 +0100

    Ajout reseau

---
+++ GIT-Reseau/Zumo_Arduino/Bat_level.cpp
@@ -0,0 +1,17 @@
+#include "Bat_level.h"
+
+float bat_level()
+{
+  return (float)analogRead(1)*1.5*5/1023; // conversion tension batterie
+}
+
+void alerteBatterie()
+{
+  float a = bat_level();
+  Serial.println("#bat_level:" + String(a) + ";");
+  if(4 < a && a < 4.7)
+  {
+    Serial.println("#lowbat;");
+  }
+}
+
