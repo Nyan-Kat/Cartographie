6432e4fba3f367c9e2bf89db1ccdc92ea4e0474f
Nyan-Kat <thomas.cusson@hotmail.fr>
2017-01-02 17:45:08 +0100

    Programme MAJ_distance

--- Affichage distance.py
+++ Affichage distance.py
@@ -2,35 +2,49 @@
 # -*- coding: utf-8 -*-
 
 from Tkinter import *
+import serial
+ser=serial.Serial('/dev/ttyACM0', 9600)
+
 
 class Interface(Frame):
 
 
-    def __init__(self, fenetre, **kwargs):
-        Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
+    def __init__(self, root,  **kwargs):
+        Frame.__init__(self, root, **kwargs)
+        
         self.pack(fill=BOTH)
-        self.nb_clic = 0
+        self.arret = True
 
         # Creation de nos widgets
-        self.message = Label(self, text="Vous n'avez pas cliqué sur le bouton.")
-        self.message.pack()
 
-        self.bouton_quitter = Button(self, text="Quitter", command=self.quit)
-        self.bouton_quitter.pack(side="left")
+        self.frequence = Label(self, text= "Affichage de la mesure.")
+        self.frequence.pack(side="top", padx=5, pady=5)
 
-        self.bouton_cliquer = Button(self, text="Cliquez ici", fg="red",
-                command=self.cliquer)
-        self.bouton_cliquer.pack(side="right")
+        self.bouton_cliquer = Button(self, text="Demarrer", command=self.demarrer)
+        self.bouton_cliquer.pack(side="left", padx=5, pady=5)
+        
+        self.bouton_stop = Button(self, text="Stop", command=self.stop)
+        self.bouton_stop.pack(side="right", padx=5, pady=5)
 
-    def cliquer(self):
-        """Il y a eu un clic sur le bouton.
+    def demarrer(self):
+        self.arret = False
+        self.MAJ_frequence()
+        
+    def stop(self):
+        self.arret = True
+        
+    def MAJ_frequence(self):
+        if self.arret == False:
+            resultat = str(ser.readline())
+            resultat = resultat.strip()
+            self.frequence["text"] = "La distance entre les deux cartes est de {}.".format(resultat)
+            self.update()
+            self.after(50, self.MAJ_frequence)
+ 
 
-        On change la valeur du label message."""
+root = Tk()
+root.title('Mesure distance UWB')
+root.geometry('350x70+500+300')
 
-        self.nb_clic += 1
-        self.message["text"] = "Vous avez cliqué {} fois.".format(self.nb_clic)
-
-fenetre = Tk("toto")
-interface = Interface(fenetre)
-
+interface = Interface(root)
 interface.mainloop()
