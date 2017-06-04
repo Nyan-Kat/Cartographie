afb6fa118043f1afe03bafffa4745e6d60006a6a
fhsup <billi2014@outlook.fr>
2017-06-04 23:18:45 +0200

    nouveau fichier : installation_librairies/installation_opencv.sh 	nouveau fichier : installation_librairies/installation_tbb.sh

---
+++ installation_librairies/installation_opencv.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#https://www.theimpossiblecode.com/blog/intel-tbb-on-raspberry-pi/
+#https://www.pyimagesearch/2015/10/26/how-to-install-opencv-3-on-raspbian-jessie/
+
+
+sudo apt-get install build-essential git cmake pkg-config
+sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev lib4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+
+sudo apt-get install install libgtk-3-dev
+
+sudo apt-get install install libatlas-base-dev gfortran
+
+sudo apt-get install install python2.7-dev python3.5-dev
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install numpy
+
+cd /home/pi/Downloads
+
+wget -O opencv-3.2.0.zip https://github.com/opencv/opencv/archive/3.2.0.zip 
+wget -O opencv-3.2.0-contrib.zip https://github.com/opencv/opencv_contrib/archive/3.2.0.zip
+unzip  opencv-3.2.0.zip
+unzip opencv-3.2.0-contrib.zip
+
+cd /home/pi/Downloads/opencv-3.2.0
+mkdir build
+cd build
+cmake -DBUILD_EXAMPLES=ON -DINSTALL_C_EXAMPLES=ON -D_INSTALL_PYTHON_EXAMPLES=ON -DOPENCV_EXTRA_MODULES_PATH=/home/pi/Downloads/opencv_contrib-3.2.0/modules -DCMAKE_CXX_FLAGS="-DTBB_USE_GCC_BUILTINS=1 -D__TBB_64BIT_ATOMICS=0" -DENABLE_VFPV3=ON -DENABLE_NEON=ON -DBUILD_TESTS=OFF -DWITH_TBB=ON -DWITH_GTK_2_X=ON -DCMAKE_BUILD_TYPE=Release ..
+make -j3
+
+sudo make install
+#~ sudo apt-get install checkinstall
+#~ echo "opencv 3.2.0 build_rpi3_release_fp_tbb" > description-pak
+#~ echo | sudo checkinstall -D --install=yes --pkgname=opencv --pkgversion=3.2.0 --provides=opencv --nodoc --backup=no
+
+cd /usr/local/lib/python3.5/dist-packages
+sudo mv cv2.cpython-34m.so  cv2.so
---
+++ installation_librairies/installation_tbb.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#https://www.theimpossiblecode.com/blog/intel-tbb-on-raspberry-pi/
+
+cd /home/pi/Downloads
+wget https://github.com/01org/tbb/archive/2017_U5.zip
+unzip 2017_U5.zip
+cd tbb-2017_U5
+make tbb CXXFLAGS="-DTBB_USE_GCC_BUILTINS=1 -D_TBB_64BIT_ATOMICS=0" -j3
+
+cd /home/pi/Downloads
+mkdir libtbb-dev_2017_U5_armhf
+cd libtbb-dev_2017_U5_armhf
+mkdir -p usr/local/lib/pkgconfig
+mkdir -p usr/local/include
+mkdir DEBIAN
+
+
+cd /home/pi/Downloads/libtbb-dev_2017_U5_armhf/DEBIAN
+
+
+#Insert the description
+
+echo "Package: libtbb-dev
+Priority: extra
+Section: universe/libdevel
+Maintainer: Your Name <email address>
+Architecture: armhf
+Version: 2017.5
+Homepage: http://threadingbuildingblocks.org/
+Description: parallelism library for C++ - development files
+ TBB is a library that helps you leverage multi-core processor
+ performance without having to be a threading expert. It represents a
+ higher-level, task-based parallelism that abstracts platform details
+ and threading mechanism for performance and scalability.
+ .
+ (Note: if you are a user of the i386 architecture, i.e., 32-bit Intel
+ or compatible hardware, this package only supports Pentium4-compatible
+ and higher processors.)
+ .
+ This package includes the TBB headers, libs and pkg-config" > control
+
+
+#Insert the library:
+
+
+
+cd /home/pi/Downloads/libtbb-dev_2017_U5_armhf/usr/local/lib
+cp /home/pi/Downloads/tbb-2017_U5/build/*_release/libtbb.so.2 .
+ln -s libtbb.so.2 libtbb.so
+
+
+
+#Insert the headers:
+
+
+cd /home/pi/Downloads/tbb-2017_U5/include
+cp -r serial tbb /home/pi/Downloads/libtbb-dev_2017_U5_armhf/usr/local/include
+
+
+cd /home/pi/Downloads/libtbb-dev_2017_U5_armhf/usr/local/lib/pkgconfig
+echo"
+# Manually added pkg-config file for tbb - START
+prefix=/usr/local
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+Name: tbb
+Description: thread building block
+Version: 2017.5
+Cflags: -I${includedir} -DTBB_USE_GCC_BUILTINS=1 -D__TBB_64BIT_ATOMICS=0
+Libs: -L${libdir} -ltbb
+# Manually added pkg-config file for tbb - END">tbb.pc
+
+
+#Build the package, with correct ownership:
+cd /home/pi/Downloads
+sudo chown -R root:staff libtbb-dev_2017_U5_armhf
+sudo dpkg-deb --build libtbb-dev_2017_U5_armhf
+
+#Install the package
+
+sudo dpkg -i /home/pi/Downloads/libtbb-dev_2017_U5_armhf.deb
+sudo ldconfig
